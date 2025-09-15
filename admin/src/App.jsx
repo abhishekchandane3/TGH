d5c143a8b6f25b42ad65d2a3338eb90a3e07ef89
Abhishek Chandane <abhishekchandane3@gmail.com>
2025-09-15 19:49:57 +0530

    feat: Pass API URL as prop to Add and List components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,9 @@ import List from './pages/List/List.jsx'
 import Orders from './pages/Orders/Orders.jsx'
  import { ToastContainer } from 'react-toastify';
  const App = () => {
+
+  const url = "http://localhost:4000";
+
    return (
      <div>
       <ToastContainer/>
@@ -15,9 +18,9 @@ import Orders from './pages/Orders/Orders.jsx'
        <div className='app-content'>
         <Sidebar/>
         <Routes>
-          <Route path="/add" element={ <Add/> }  />
-          <Route path="/list" element={ <List/> } />
-          <Route path="/orders" element={ <Orders/> } /> 
+          <Route path="/add" element={ <Add url={url}/>}  />
+          <Route path="/list" element={ <List url={url}/>} />
+          <Route path="/orders" element={ <Orders url={url}/>} /> 
         </Routes>
        </div>
      </div>
