bcdf2b5bc200b4c23b12cd4b50d489a752e52bd7
Abhishek Chandane <abhishekchandane3@gmail.com>
2025-09-15 19:01:25 +0530

    Changes

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,48 @@
+import foodModel from "../models/foodModel.js";
+import fs from 'fs'
+
+// Add Food Item
+const addFood =  async(req, res)=>{
+           // ✅ Safely handle file upload
+        let image_filename = req.file ? req.file.filename : null;
+
+        if (!image_filename) {
+            return res.json({ success: false, message: "Image is required" });
+        }
+
+    const food = new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image : image_filename,
+    })
+    try{
+        await food.save();
+        res.json(
+            {success : true, message : "Food Added" }
+        )
+    }catch(error){
+        console.log(error)
+        res.json(
+            {success : false, message : "   Error" }
+        )
+    }
+}
+
+// All Food List
+const listFood = async(req, res) =>{
+    try{
+        const foods = await foodModel.find({});
+        res.json(
+            {success:true, data:foods}
+        )
+    }catch(error){
+        console.log(error);
+        res.json(
+            {success:false,  message : "Error"}
+        )
+    }
+} 
+
+export {addFood, listFood}
