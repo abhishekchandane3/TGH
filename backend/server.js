41bc86e511cf88f4bfee44952429216cdb0fa09f
Abhishek Chandane <abhishekchandane3@gmail.com>
2025-09-16 19:37:24 +0530

    feat: Implement user authentication with login and registration endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import  foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+
+import 'dotenv/config.js';
 
 
 
@@ -21,6 +24,7 @@ connectDB();
 // API Endpoint
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
 
 app.get( "/", (req, res)=>{
     res.send("API IS WORKING")
--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,25 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import './LoginPopup.css'
 import {assets} from '../../assets/assets'
 
 const LoginPopup = ({setShowLogin}) => {
     const [currState,setCurrState] = useState("Login");
+
+    const [data, setData] = useState({
+        name:"",
+        email:"",
+        password:"",
+    })
+
+    const onChangeHandler = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value
+        setData( data=>( {...data,[name]:value } ) )
+    }
+
+    useEffect( ()=>{ console.log(data); }, [data] )
+
   return (
     <div className='login-popup'> 
         <form  className="login-popup-container">
@@ -14,9 +29,9 @@ const LoginPopup = ({setShowLogin}) => {
             </div>
 
             <div className="login-popup-inputs">
-                {currState==="Login"? <></> : <input type="text" placeholder='Your Name' required /> }
-                <input type="email" placeholder='Your Email' required />
-                <input type="password" placeholder='  Password' required />
+                {currState==="Login"? <></> : <input name='name' onChange={onChangeHandler}  value={data.name} type="text" placeholder='Your Name' required /> }
+                <input name='email' onChange={onChangeHandler} value={data.email}  type="email" placeholder='Your Email' required />
+                <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='  Password' required />
             </div>
 
             <button  > {currState==="Sign Up"?"Create Account":"Login"} </button>
