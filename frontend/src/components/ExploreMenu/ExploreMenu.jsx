7191989a99779ee31f140113039ca4a59267c925
Abhishek Chandane <abhishekchandane3@gmail.com>
2025-09-18 21:30:04 +0530

    style: Update ExploreMenu and ProductSlide styles for improved layout and aesthetics

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -14,28 +14,51 @@ const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore Our Menu/ Products</h1>
-        <p className=''>
+        <p className='explore-menu-text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
 
-        <div className='explore-menu-list'>
+            {/* Left Button */}
 
-            {menu_list.map( (item, index)=>{
-                return( 
-                        <div onClick={ ()=> setCategory(prev=>prev===item.menu_name?"All" : item.menu_name) } 
-                            key={index}  className='explore-menu-list-item'>
-                            <img className={ category===item.menu_name?"active": "" } src={item.menu_image} alt=""    />
-                            <p>
-                                {item.menu_name}
-                            </p>
+            <div className='explore-menu-scroll-div-left'>
+                <button 
+                    className="scroll-btn left" 
+                    onClick={() => document.querySelector('.explore-menu-list').scrollBy({left: -200, behavior: 'smooth'})}
+                >
+                    &#10094;
+                </button>
+            </div>
 
-                        </div>
-                     
-                )
-            })}
+            {/* Menu List */}
+            <div className="explore-menu-list">
+                {menu_list.map((item, index) => (
+                <div
+                    onClick={() =>
+                    setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)
+                    }
+                    key={index}
+                    className="explore-menu-list-item"
+                >
+                    <img
+                    className={category === item.menu_name ? "active" : ""}
+                    src={item.menu_image}
+                    alt=""
+                    />
+                    <p>{item.menu_name}</p>
+                </div>
+                ))}
+            </div>
 
-        </div>
-            <hr />
+            {/* Right Button */}
+            <div className='explore-menu-scroll-div-left'>
+                <button 
+                    className="scroll-btn right" 
+                    onClick={() => document.querySelector('.explore-menu-list').scrollBy({left: 200, behavior: 'smooth'})}
+                >
+                    &#10095;
+                </button> 
+            </div>
+        <hr />
     </div>
   )
 }
