754e3a61a877c0c0763b39ef26b7b4d37f917944
Abhishek Chandane <abhishekchandane3@gmail.com>
2025-09-23 20:41:21 +0530

    Implement order placement functionality and update environment variables

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/userRoute.js"
 
 import 'dotenv/config.js';
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 
@@ -27,6 +28,7 @@ app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get( "/", (req, res)=>{
     res.send("API IS WORKING")
