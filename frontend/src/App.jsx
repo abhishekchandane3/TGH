6ed462b8443a3929022dc2d07bb6c5ac905d00b9
abhishekchandane3 <75213436+abhishekchandane3@users.noreply.github.com>
2025-09-25 03:02:55 +0530

    Update App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,39 @@
+import React, { useState } from 'react'
+// import Navbar from './components/Navbar/Navbar'
+import Navbar2 from './components/Navbar/Navbar2'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import Footer from './components/Footer/Footer'
+import Verify from './pages/Verify/verify'
+import MyOrders from './pages/MyOrders/MyOrders'
 
+const App = () => {
+
+
+  const [showLogin, setShowLogin] = useState(false);
+
+  return (
+      <>
+        { showLogin?<LoginPopup setShowLogin={setShowLogin}/> : <></> }
+          <div className='app'>
+              {/* <Navbar setShowLogin={setShowLogin} /> */}
+               <Navbar2 setShowLogin={setShowLogin} />
+
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/cart' element={<Cart/>}/>
+                <Route path='/order' element={<PlaceOrder/>}/>
+                <Route path='/verify' element={<Verify/>}/>
+                <Route path='/myorders' element={<MyOrders/>}/>
+              </Routes>
+          </div>
+          <Footer/>
+      </>
+
+  )
+}
+
+export default App
