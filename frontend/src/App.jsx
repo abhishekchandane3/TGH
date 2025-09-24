9cb6f08a3cb40d01f6e96d4752feaa4c065fd96e
abhishekchandane3 <75213436+abhishekchandane3@users.noreply.github.com>
2025-09-25 02:51:43 +0530

    Update App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,8 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Footer from './components/Footer/Footer'
-import Verify from './pages/Verify/Verify'
+import Verify from './pages/Verify/verify'
+import MyOrders from './pages/MyOrders/MyOrders'
 
 const App = () => {
 
@@ -26,6 +27,7 @@ const App = () => {
                 <Route path='/cart' element={<Cart/>}/>
                 <Route path='/order' element={<PlaceOrder/>}/>
                 <Route path='/verify' element={<Verify/>}/>
+                <Route path='/myorders' element={<MyOrders/>}/>
               </Routes>
           </div>
           <Footer/>
