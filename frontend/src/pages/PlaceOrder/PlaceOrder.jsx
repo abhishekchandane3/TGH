2ac8c104d6d6b71ef9f3cd512a51450ec07bf3cb
abhishekchandane3 <75213436+abhishekchandane3@users.noreply.github.com>
2025-09-25 02:40:33 +0530

    Update PlaceOrder jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -5,6 +5,8 @@ import React, { useContext, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'; 
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PlaceOrder = () => {
 
@@ -56,6 +58,16 @@ const PlaceOrder = () => {
       alert("Error")
     }
   }
+
+  const navigate =  useNavigate();
+
+  useEffect(()=>{
+    if(!token){
+      navigate('/cart');
+    }else if(getTotalCartAmount() === 0){
+      navigate('/cart');
+    }
+  },[])
 
   return (
     <div className='place-order-container'>
